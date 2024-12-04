edfd799ecb73889fa01af32a8d076f97f5633dbd
Marvin López Osorio <majoloso97@gmail.com>
2024-12-05 00:37:30 +0100

    feat: create for part 2

---
+++ 2024/day-4/part-2.py
@@ -0,0 +1,49 @@
+def is_xmas(input, row, col, rows, cols):
+    if (row == 0) or (col == 0) or (rows - row == 1) or (cols - col == 1):
+        return False
+
+    downward = input[row - 1][col - 1] + "A" + input[row + 1][col + 1]
+    upward = input[row - 1][col + 1] + "A" + input[row + 1][col - 1]
+
+    # print(f"Position {row}, {col}. Downward: {downward} | Upward: {upward}")
+
+    if (downward == "MAS" or downward == "SAM") and (
+        upward == "MAS" or upward == "SAM"
+    ):
+        return True
+    return False
+
+
+def get_input(filepath):
+    lines = []
+    with open(filepath) as f:
+        for line in f:
+            lines.append(line)
+    return lines
+
+
+input = get_input("input.txt")
+# input = [
+#     "MMMSXXMASM",
+#     "MSAMXMSMSA",
+#     "AMXSXMAAMM",
+#     "MSAMASMSMX",
+#     "XMASAMXAMM",
+#     "XXAMMXXAMA",
+#     "SMSMSASXSS",
+#     "SAXAMASAAA",
+#     "MAMMMXMMMM",
+#     "MXMXAXMASX",
+# ]
+
+counter = 0
+rows = len(input)
+for row in range(rows):
+    cols = len(input[row])
+    for col in range(cols):
+        if input[row][col] != "A":
+            continue
+        if is_xmas(input, row, col, rows, cols):
+            counter += 1
+
+print(counter)
